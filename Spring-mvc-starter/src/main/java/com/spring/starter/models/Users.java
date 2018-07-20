bb9989b52641f075494e1fef2c81623a68433444
john <john@IGZ20004JOHNX>
2018-07-20 22:00:22 +0530

    first commit

---
+++ Spring-mvc-starter/src/main/java/com/spring/starter/models/Users.java
@@ -0,0 +1,13 @@
+package com.spring.starter.models;
+
+public class Users {
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
